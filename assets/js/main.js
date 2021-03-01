4490145a2790a1c96eae32abb279f416d323389c
Vsevolod <cerneavschivsevolod@gmail.com>
2021-03-01 15:50:17 +0100

    Esercizio dopo la correzione

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,17 @@
 var km = parseInt(prompt("Quanti chilometri vuoi percorere?"));
 var eta = parseInt(prompt("Quanti anni hai?"));
 
-// Calcoliamo il costo in base ai chilometri
-var distanzaEuro = km * 0.21;
 
-if(isNaN(eta) || isNaN(km)) {
+
+
+if  (isNaN(eta) || isNaN(km) ) {
   prompt("Reinserisci i campi correttamante");
-}else if (eta < 18) {  // Veddiamo se l'età entra nelle casistiche di sconto e stampiamo il risultato;
+} else if (eta < 18) {  // Veddiamo se l'età entra nelle casistiche di sconto e stampiamo il risultato;
+  var distanzaEuro = km * 0.21; // Calcoliamo il costo in base ai chilometri
   prezzoMinorenni = distanzaEuro - (distanzaEuro * 0.2);   // sconto minorenni di 20%
   document.getElementById("costoBiglietto").innerHTML = "Il prezzo del biglietto è: " + prezzoMinorenni.toFixed(2) + " euro.";
 } else if (eta > 65) {
+  console.log(distanzaEuro);
   prezzoOver65 = distanzaEuro - (distanzaEuro * 0.4); // sconto over 65 di 40%
   document.getElementById("costoBiglietto").innerHTML = "Il prezzo del biglietto è: " + prezzoOver65.toFixed(2) + " euro.";
 } else {
